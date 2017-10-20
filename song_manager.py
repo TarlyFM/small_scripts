cf2629c7f5cebcabf9a9d12fa1a787d063bfc5fe
Theo Massard <massar_t@etna-alternance.net>
2017-10-20 23:55:23 +0200

    Huge cleanup I must say

--- song_manager.py
+++ song_manager.py
@@ -3,12 +3,8 @@ Song manager
 
 Ensure downloading, cleaning and storing songs.
 """
-import os
-import urllib
-import re
-from subprocess import call
 import youtube_dl
 
 
-def downloader(link, callback, *args):
+def downloader(link, callback, **args):
     pass
