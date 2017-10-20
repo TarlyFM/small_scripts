7e05af8c2e7499e8cbbfb51c168bfb16af128e6f
Theo Massard <massar_t@etna-alternance.net>
2017-10-20 23:54:51 +0200

    Rename file Cleanup

---
+++ song_manager.py
@@ -0,0 +1,14 @@
+"""
+Song manager
+
+Ensure downloading, cleaning and storing songs.
+"""
+import os
+import urllib
+import re
+from subprocess import call
+import youtube_dl
+
+
+def downloader(link, callback, *args):
+    pass
