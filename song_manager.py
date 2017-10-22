65cdb42513b9e2bfe0a7f4cc75441bcbeb9ee73c
Deville Matthew <kemiimek@live.fr>
2017-10-23 00:26:45 +0200

    Setting up youtube-dl

--- song_manager.py
+++ song_manager.py
@@ -5,6 +5,26 @@ Ensure downloading, cleaning and storing songs.
 """
 import youtube_dl
 
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'aac',
+        'preferredquality': '320',
+    }],
+    'logger': MyLogger(),
+}
 
 def downloader(link, callback, **args):
-    pass
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
