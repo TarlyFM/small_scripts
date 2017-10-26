e792db11d0e1801adfe1ceb62293d86aa77c0a69
Deville Matthew <kemiimek@live.fr>
2017-10-26 22:05:15 +0200

    Modify song_manager.py

--- song_manager.py
+++ song_manager.py
@@ -8,7 +8,8 @@ import os
 import sys
 import subprocess
 
-_DESTINATION = os.environ['HOME'] + "/Music/"
+_DEFAULT_PATH = "/tmp/youtube_dl/"
+_DEFAULT_OUTPUT = os.environ['HOME'] + '/Music'
 
 class MyLogger(object):
     def debug(self, msg):
@@ -22,7 +23,7 @@ class MyLogger(object):
 
 def my_hook(d):
     if d['status'] == 'error':
-        pass
+        raise DownloadError("Ma bite")
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -32,24 +33,46 @@ ydl_opts = {
         'preferredquality': '320',
     }],
     'logger': MyLogger(),
-    'outtmpl': '/tmp/youtube_dl/%(title)s-%(id)s.%(ext)s',
+    'outtmpl': _DEFAULT_PATH + '%(title)s-%(id)s.%(ext)s',
     'progress_hooks': [my_hook],
     'noplaylist': True,
 }
 
-def downloader(link, callback=None, **args):
+def download(link, destination=_DEFAULT_PATH):
     """
     Where the magic happens
     """
-    if not os.path.exists(_DESTINATION):
-        os.makedirs(_DESTINATION)
+    result = ""
+
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
-    for file in os.listdir('/tmp/youtube_dl/'):
-        output = '"' + _DESTINATION + file + '"'
-        ffmpeg = 'ffmpeg -i "/tmp/youtube_dl/' + file + '" -af silenceremove=1:0:-50dB ' + output
-        os.system(ffmpeg)
+        temp = ydl.extract_info(link)
+        result = os.path.splitext(ydl.prepare_filename(temp))[0] + ".mp3"
+
+    return (result)
+
+def postprocess(src, dest_dir = _DEFAULT_OUTPUT):
+    """
+    Calls ffmpeg and removes scilences
+    """
+    def shellquote(s):
+        return "'" + s.replace("'", "'\\''") + "'"
+
+    #src = shellquote(src)
+    #dest_dir = shellquote(dest_dir)
+    destination = os.path.join(dest_dir, os.path.basename(src))
+    ffmpeg = 'ffmpeg -i "' + src + '" -af silenceremove=1:0:-50dB "' + destination + '"'
+    os.system(ffmpeg)
+    os.system("rm " + src)
+
+    return (destination)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        downloader(sys.argv[1])
+        res = download(sys.argv[1])
+        print (res)
+        res = postprocess(res)
+        print (res)
