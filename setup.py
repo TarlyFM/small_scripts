68526fa778df7c65f197656923c5d87c9967dd15
Theophile VALLEE <tvallee@student.42.fr>
2017-11-02 07:14:24 +0100

    Packaging (#1)
    Fix packaging

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='songmanager',
       version='0.1',
       description='Download songs and process them',
-      url='http://github.com/tarlyfm/UDONscripts',
+      url='https://www.github.com/UDONRadio/UDON_scripts',
       author='Matthew Deville',
       author_email='kemiimek@live.fr',
       license='MIT',
-      packages=['songmanager'],
+      packages=['UDON_scripts'],
       install_requires=['youtube_dl'],
       zip_safe=False)
