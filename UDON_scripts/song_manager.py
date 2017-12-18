8ae631fe993693d3f24ac7486a71c884c4a84292
Deville Matthew <kemiimek@live.fr>
2017-12-18 11:23:39 +0100

    Add taggin utilities

--- UDON_scripts/song_manager.py
+++ UDON_scripts/song_manager.py
@@ -4,6 +4,7 @@ Song manager
 Ensure downloading, cleaning and storing songs.
 """
 import youtube_dl
+import eyed3
 import os
 import sys
 import subprocess
@@ -73,6 +74,31 @@ def postprocess(src, dest_dir=_DEFAULT_OUTPUT):
     os.system("rm " + shellquote(src))
     return (destination)
 
+def set_tag(path, artist, title, album = None, genre = None):
+    """
+    Tags with EyeD3 songs with given parameters
+    """
+    audiofile = eyed3.load(path)
+    audiofile.tag.artist = artist
+    audiofile.tag.album = album
+    audiofile.tag.genre = genre
+    audiofile.tag.title = tile
+    audiofile.tag.save()
+
+def get_tags(path):
+    """
+    Gets audio tags if any is available
+    Return value: a dictionnary containing all info on the file
+    """
+    result = {}
+    trackInfo = eyeD3.Mp3AudioFile(path)
+    tag = trackInfo.getTag()
+    result["path"] = tag.link(path)
+    result["artist"] = tag.getArtist()
+    result["album"] = tag.getAlbum()
+    result["title"] = tag.getTitle()
+    result["play_time"] = datetime.timedelta(seconds=trackInfo.time_secs)
+    return (result)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
